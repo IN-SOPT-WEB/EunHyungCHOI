158d8d033948179631610284e34462bae7b0fcdd
Eunhyung Choi <milkiss0414@naver.com>
2022-10-11 16:00:50 +0900

    Update: 태그 중복시 미추가 기능

--- week2/velog/tag.js
+++ week2/velog/tag.js
@@ -6,13 +6,22 @@ const tags = document.querySelector(".js-tags");
 
 function handleTagSubmit(e) {
     e.preventDefault();
+    const todoIndex = Array.from(
+        e.target.parentElement.parentElement.children
+    ).indexOf(e.target.parentElement);
 
     const newTag = document.createElement("li");
+
+    const tagList = Array.from(tags.children).map((child) => 
+        child.innerHTML
+    ); // 태그 스트링만 담은 리스트
 
     newTag.classList.add("tags__tag");
     newTag.innerHTML = tagInput.value;
 
-    tags.appendChild(newTag);
+    if(!tagList.includes(newTag.innerHTML)){
+        tags.appendChild(newTag);
+    }
 };
 
 function init() {
