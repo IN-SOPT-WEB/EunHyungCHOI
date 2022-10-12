faba917f504300c103037fa88deb6d9b186a297e
Eunhyung Choi <milkiss0414@naver.com>
2022-10-12 23:13:47 +0900

    Refact: 태그 추가 함수 따로 선언

--- week2/velog/write.js
+++ week2/velog/write.js
@@ -3,37 +3,36 @@ const tagForm = document.querySelector(".js-form-tag"),
 
 const tags = document.querySelector(".js-tags");
 
+function addNewTag(str) {
+    const newTag = document.createElement("li");
+
+    newTag.classList.add("tags__tag");
+    newTag.innerHTML = str;
+    newTag.addEventListener("click", (e) => tags.removeChild(e.currentTarget));
+
+    tags.appendChild(newTag);
+}
+
 function handleTagSubmit(e) {
     e.preventDefault();
 
-    const newTag = document.createElement("li");
+    const newTagStr = tagInput.value;
     const tagList = Array.from(tags.children).map((child) => 
         child.innerHTML
     ); // 태그 스트링만 담은 리스트
 
-    newTag.classList.add("tags__tag");
-    newTag.innerHTML = tagInput.value;
-    newTag.addEventListener("click", (e) => tags.removeChild(e.currentTarget));
+    if(!tagList.includes(newTagStr)){
+        addNewTag(newTagStr);
 
-    if(!tagList.includes(newTag.innerHTML)){
-        tags.appendChild(newTag);
         // 로컬 스토리지에 스트링 배열 저장
-        tagList.push(newTag.innerHTML);
+        tagList.push(newTagStr);
         localStorage.setItem('tags', tagList);
     }
 };
 
 function init() {
     const storedTags = localStorage.getItem('tags').split(',');
-    storedTags.forEach((tagStr) => {
-        const newTag = document.createElement("li");
-
-        newTag.classList.add("tags__tag");
-        newTag.innerHTML = tagStr;
-        newTag.addEventListener("click", (e) => tags.removeChild(e.currentTarget));
-
-        tags.appendChild(newTag);
-    });
+    storedTags.forEach((tagStr) => addNewTag(tagStr));
 
     tagForm.addEventListener("submit", handleTagSubmit);
 };
