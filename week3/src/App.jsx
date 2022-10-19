abf351a34b1c6ca8b041e94491868abb9f05c86d
Eunhyung Choi <milkiss0414@naver.com>
2022-10-19 14:00:53 +0900

    Feat: 이미지 추가

--- week3/src/App.jsx
+++ week3/src/App.jsx
@@ -1,21 +1,12 @@
 import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Time from "./components/Time";
+import Content from "./components/Content";
 import styled from "styled-components";
-
-const Title = styled.h1`
-  color: red;
-`;
 
 function App() {
   return (
     <div className="App">
-      <Header data={"오늘"}/>
-      <Nav/>
-      <Time/>
-      <Title>
-        Hello World
-      </Title>
+      <Header/>
+      <Content/>
     </div>
   );
 }
