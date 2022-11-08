3cd616a11d11861080a49774218f71e8c71befb5
Eunhyung Choi <milkiss0414@naver.com>
2022-11-09 00:47:43 +0900

    Feat: SearchBar UI

--- week4/src/components/SearchBar.jsx
+++ week4/src/components/SearchBar.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 80%;
+  height: 20vh;
+  background-color: #6c00b9a3;
+  border-radius: 25px;
+`;
+
+const Title = styled.h1`
+  color: white;
+`;
 
 export default function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <Box>
+      <Title>🐈‍ Github Profile Cat</Title>
+      <form>
+        <input type="text" placeholder="Github Username..." />
+      </form>
+    </Box>
+  );
 }
--- week4/src/pages/SearchPage.jsx
+++ week4/src/pages/SearchPage.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import Result from "../components/Result";
 import SearchBar from "../components/SearchBar";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 export default function SearchPage() {
   const [userId, setUserId] = useState("");
 
   return (
-    <div>
+    <Wrapper>
       <SearchBar />
       <Result />
-    </div>
+    </Wrapper>
   );
 }
