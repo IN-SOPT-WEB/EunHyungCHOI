34876e4cdcfdf84a742f69682394f9d7a6d83da4
Eunhyung Choi <milkiss0414@naver.com>
2022-11-09 16:45:16 +0900

    Update: Result style 수정

--- week4/src/components/Result.jsx
+++ week4/src/components/Result.jsx
@@ -15,13 +15,38 @@ const Box = styled.div`
 `;
 
 const Image = styled.img`
-  width: 200px;
-  height: 200px;
-
-  background-color: black;
+  width: 150px;
 `;
 
-const Button = styled.button``;
+const Names = styled.div`
+  text-align: center;
+  line-height: 30px;
+  & > h1 {
+    font-size: 20px;
+    margin-top: 5px;
+    color: white;
+  }
+  & > h2 {
+    font-size: 18px;
+    color: wheat;
+  }
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  margin: 10px 0px;
+  background-color: rgba(0, 0, 0, 0);
+  color: white;
+  border: 2px solid white;
+  border-radius: 20px;
+
+  &:hover {
+    color: #6c00b9a3;
+    background-color: white;
+    transition-duration: 0.2s;
+    cursor: pointer;
+  }
+`;
 
 const Circles = styled.div`
   display: flex;
@@ -35,35 +60,39 @@ const Circle = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-
+  margin-top: 10px;
   width: 100px;
   height: 100px;
   border: 2px solid white;
   border-radius: 50%;
 
   color: white;
+
+  & > h3 {
+    font-size: 14px;
+  }
+  & > p {
+    font-size: 24px;
+    margin-top: 5px;
+  }
 `;
 
 export default function Result({ userInfo }) {
   return (
     <Box>
-      <Image />
-      <p>{userInfo.userName}</p>
-      <p>{userInfo.userId}</p>
+      <Image src="github-icon.png" />
+      <Names>
+        <h1>{userInfo.userName}</h1>
+        <h2>{userInfo.userId}</h2>
+      </Names>
       <Button>Visit {userInfo.userName}</Button>
       <Circles>
-        <Circle>
-          <div>Followers</div>
-          <div>{userInfo.details.followers}</div>
-        </Circle>
-        <Circle>
-          <div>Followings</div>
-          <div>{userInfo.details.followings}</div>
-        </Circle>
-        <Circle>
-          <div>Repos</div>
-          <div>{userInfo.details.repos}</div>
-        </Circle>
+        {["Followers", "Followings", "Repos"].map((category) => (
+          <Circle>
+            <h3>{category}</h3>
+            <p>{userInfo.details[category.toLowerCase()]}</p>
+          </Circle>
+        ))}
       </Circles>
     </Box>
   );
