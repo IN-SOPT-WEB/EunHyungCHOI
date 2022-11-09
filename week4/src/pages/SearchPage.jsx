7e45bcea783d1f0a7d71093750faf9ef945240e5
Eunhyung Choi <milkiss0414@naver.com>
2022-11-09 17:08:46 +0900

    Update: API 통신 렌더링 성공

--- week4/src/pages/SearchPage.jsx
+++ week4/src/pages/SearchPage.jsx
@@ -17,10 +17,10 @@ const Wrapper = styled.div`
 export default function SearchPage() {
   const [userId, setUserId] = useState("ilmerry");
   const [userInfo, setUserInfo] = useState({
-    image: "이미지",
+    imageUrl: "이미지",
     userName: "USERNAME",
     userId: "userid",
-    url: "github 주소",
+    githubUrl: "github 주소",
     details: {
       followers: 0,
       followings: 0,
@@ -29,9 +29,35 @@ export default function SearchPage() {
   });
 
   useEffect(() => {
-    axios.get(`${API_URL}/${userId}`).then((res) => {
-      console.log(res);
-    });
+    axios
+      .get(`${API_URL}/${userId}`)
+      .then((res) => {
+        console.log(res.data);
+        const {
+          avater_url,
+          name,
+          login,
+          html_url,
+          followers,
+          following,
+          public_repos,
+        } = res.data;
+
+        setUserInfo({
+          imageUrl: avater_url,
+          userName: name,
+          userId: login,
+          githubUrl: html_url,
+          details: {
+            followers: followers,
+            followings: following,
+            repos: public_repos,
+          },
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, [userId]);
 
   return (
