5d3e92b9ad91f62852b86cb58b8cc040236a0c85
Eunhyung Choi <milkiss0414@naver.com>
2022-11-26 14:14:16 +0900

    Refact: postId

---
+++ week4/src/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function MainPage() {
+  return (
+    <div>
+      <Link to="/search">깃헙 프로필 보러가기</Link>
+    </div>
+  );
+}
